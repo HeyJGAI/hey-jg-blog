a70daee9388173eee2d6a2bfd96ada29ce4de2fb
HeyJGAI <heyjg.ai@gmail.com>
2026-08-06 15:59:05 +0530

    RE-RE Remove DS

--- Hey-JG-Blog/src/components/AdminHeader.jsx
+++ Hey-JG-Blog/src/components/AdminHeader.jsx
@@ -101,7 +101,7 @@ function AdminHeader({ activeSection }) {
                             <div className="space-y-8 md:space-y-12 w-full max-w-lg">
                                 {menuItems.map((item, index) => (
                                     <div key={item.section} className="text-center">
-                                        
+                                        <a
                                             href={item.href}
                                             className={`relative inline-block text-4xl sm:text-5xl md:text-7xl transition-all duration-300 font-extralight tracking-wide ${
                                                 activeSection === item.section
